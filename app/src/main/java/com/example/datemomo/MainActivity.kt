e1932df8f6db2e8920114c8416d9f1ef73be09ae
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2022-05-03 00:20:56 +0300

    Derived the application class to hold the two methods that will be called in all the activities to darken the icons in status bar and navigation bar

--- app/src/main/java/com/example/datemomo/MainActivity.kt
+++ app/src/main/java/com/example/datemomo/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.datemomo
 
-import android.graphics.Color
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
-import android.view.View
-import android.view.Window
-import android.view.WindowInsetsController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.datemomo.MainApplication.Companion.setNavigationBarDarkIcons
+import com.example.datemomo.MainApplication.Companion.setStatusBarDarkIcons
 import com.example.datemomo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -48,46 +45,6 @@ class MainActivity : AppCompatActivity() {
             } else {
                 binding.passwordInput.leftIconInputLayout.background = ContextCompat.getDrawable(this, R.drawable.normal_edit_text)
                 binding.passwordInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_password))
-            }
-        }
-    }
-
-    @Suppress("DEPRECATION")
-    fun Window.setStatusBarDarkIcons(dark: Boolean) {
-        when {
-            Build.VERSION_CODES.R <= Build.VERSION.SDK_INT -> insetsController?.setSystemBarsAppearance(
-                if (dark) WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS else 0,
-                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-            )
-            Build.VERSION_CODES.M <= Build.VERSION.SDK_INT -> decorView.systemUiVisibility = if (dark) {
-                decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            } else {
-                decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-            }
-            else -> if (dark) {
-                // dark status bar icons not supported on API level below 23, set status bar
-                // color to black to keep icons visible
-                statusBarColor = Color.BLACK
-            }
-        }
-    }
-
-    @Suppress("DEPRECATION")
-    fun Window.setNavigationBarDarkIcons(dark: Boolean) {
-        when {
-            Build.VERSION_CODES.R <= Build.VERSION.SDK_INT -> insetsController?.setSystemBarsAppearance(
-                if (dark) WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS else 0,
-                WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-            )
-            Build.VERSION_CODES.O <= Build.VERSION.SDK_INT -> decorView.systemUiVisibility = if (dark) {
-                decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-            } else {
-                decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
-            }
-            else -> if (dark) {
-                // dark navigation bar icons not supported on API level below 26, set navigation bar
-                // color to black to keep icons visible
-                navigationBarColor = Color.BLACK
             }
         }
     }
