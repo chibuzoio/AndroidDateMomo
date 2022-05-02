6add7d576b01ddee8acd526899f27e2b5d276bbb
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2022-05-02 17:29:58 +0300

    Perfected all input field components

--- app/src/main/java/com/example/datemomo/MainActivity.kt
+++ app/src/main/java/com/example/datemomo/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.datemomo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.datemomo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+/*        binding.nameInput.genericInputField.setOnFocusChangeListener {view, focused ->
+            if (focused) {
+                view.background = ContextCompat.getDrawable(this, R.drawable.focused_edit_text)
+            } else {
+                view.background = ContextCompat.getDrawable(this, R.drawable.normal_edit_text)
+            }
+        }
+
+        binding.sexInput.genericInputField.setOnFocusChangeListener {view, focused ->
+            if (focused) {
+                view.background = ContextCompat.getDrawable(this, R.drawable.focused_edit_text)
+            } else {
+                view.background = ContextCompat.getDrawable(this, R.drawable.normal_edit_text)
+            }
+        }*/
     }
 }
 
