805cbf7bff2203d18687b667f9d9f0653fc7d1ee
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2022-05-03 21:37:16 +0300

    Completed form field validation and posting of data to the server

--- app/src/main/java/com/example/datemomo/MainActivity.kt
+++ app/src/main/java/com/example/datemomo/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.datemomo
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.text.method.PasswordTransformationMethod
 import android.util.Log
+import android.view.View
+import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import com.example.datemomo.MainApplication.Companion.setNavigationBarDarkIcons
 import com.example.datemomo.MainApplication.Companion.setStatusBarDarkIcons
 import com.example.datemomo.databinding.ActivityMainBinding
@@ -17,8 +22,13 @@ import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
+    private var userNameValid = false
+    private var passwordValid = false
+    private lateinit var password: String
+    private lateinit var userName: String
     private lateinit var binding: ActivityMainBinding
-    private lateinit var userNameArray: Array<UserNameModel>
+    private lateinit var buttonClickEffect: AlphaAnimation
+    private var userNameArray: Array<UserNameModel> = emptyArray()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +38,8 @@ class MainActivity : AppCompatActivity() {
 
         window.setStatusBarDarkIcons(true)
         window.setNavigationBarDarkIcons(true)
+
+        buttonClickEffect = AlphaAnimation(1f, 0f)
 
         fetchUserNames()
 
@@ -39,10 +51,56 @@ class MainActivity : AppCompatActivity() {
         binding.passwordInput.leftIconInputField.genericInputField.transformationMethod =
             PasswordTransformationMethod.getInstance()
 
+        binding.createAccountSubmit.blueButtonLayout.setOnClickListener {
+            binding.createAccountSubmit.blueButtonLayout.startAnimation(buttonClickEffect)
+
+            password = binding.passwordInput.leftIconInputField.genericInputField.text.toString().trim()
+            userName = binding.userNameInput.leftIconInputField.genericInputField.text.toString().trim()
+            validatePassword(password)
+            validateUserName(userName)
+
+            if (passwordValid && userNameValid) {
+                registerUser()
+            }
+        }
+
+        binding.userNameInput.leftIconInputField.genericInputField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.userNameInput.leftIconInputLayout.background = ContextCompat.getDrawable(baseContext, R.drawable.focused_edit_text)
+                binding.userNameInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(baseContext, R.drawable.icon_person_blue))
+                binding.userNameInputError.visibility = View.GONE
+            }
+        })
+
+        binding.passwordInput.leftIconInputField.genericInputField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.passwordInput.leftIconInputLayout.background = ContextCompat.getDrawable(baseContext, R.drawable.focused_edit_text)
+                binding.passwordInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(baseContext, R.drawable.icon_password_blue))
+                binding.passwordInputError.visibility = View.GONE
+            }
+        })
+
         binding.userNameInput.leftIconInputField.genericInputField.setOnFocusChangeListener { _, focused ->
             if (focused) {
                 binding.userNameInput.leftIconInputLayout.background = ContextCompat.getDrawable(this, R.drawable.focused_edit_text)
                 binding.userNameInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_person_blue))
+                binding.userNameInputError.visibility = View.GONE
             } else {
                 binding.userNameInput.leftIconInputLayout.background = ContextCompat.getDrawable(this, R.drawable.normal_edit_text)
                 binding.userNameInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_person))
@@ -53,10 +111,60 @@ class MainActivity : AppCompatActivity() {
             if (focused) {
                 binding.passwordInput.leftIconInputLayout.background = ContextCompat.getDrawable(this, R.drawable.focused_edit_text)
                 binding.passwordInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_password_blue))
+                binding.passwordInputError.visibility = View.GONE
             } else {
                 binding.passwordInput.leftIconInputLayout.background = ContextCompat.getDrawable(this, R.drawable.normal_edit_text)
                 binding.passwordInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_password))
             }
+        }
+    }
+
+    private fun validatePassword(password: String) {
+        var errorType = "Password is too short"
+
+        if (password.length > 4) {
+            passwordValid = true
+        } else {
+            binding.passwordInput.leftIconInputLayout.background = ContextCompat.getDrawable(this, R.drawable.error_edit_text)
+            binding.passwordInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_password_red))
+            binding.passwordInputError.visibility = View.VISIBLE
+
+            if (password.isEmpty()) {
+                errorType = "Password field is empty"
+            }
+
+            binding.passwordInputError.text = errorType
+            passwordValid = false
+        }
+    }
+
+    private fun validateUserName(userName: String) {
+        var errorType = "User name is too short"
+
+        if (userName.length > 3) {
+            userNameValid = true
+
+            for (userNameModel in userNameArray) {
+                if (userNameModel.userName.lowercase() == userName.lowercase()) {
+                    errorType = "User name is already taken"
+                    userNameValid = false
+                    break
+                }
+            }
+        } else {
+            userNameValid = false
+        }
+
+        if (!userNameValid) {
+            binding.userNameInput.leftIconInputLayout.background = ContextCompat.getDrawable(this, R.drawable.error_edit_text)
+            binding.userNameInput.leftIconInputImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_person_red))
+            binding.userNameInputError.visibility = View.VISIBLE
+
+            if (userName.isEmpty()) {
+                errorType = "User name field is empty"
+            }
+
+            binding.userNameInputError.text = errorType
         }
     }
 
@@ -90,12 +198,12 @@ class MainActivity : AppCompatActivity() {
             0,
             "",
             "",
-            "userName",
+            userName,
             "",
             "",
             "",
             "",
-            "password",
+            password,
             "",
             "",
             ""
@@ -109,7 +217,7 @@ class MainActivity : AppCompatActivity() {
 
         val client = OkHttpClient()
         val request: Request = Request.Builder()
-            .url(getString(R.string.date_momo_api))
+            .url(getString(R.string.date_momo_api) + "service/registermember.php")
             .post(requestBody)
             .build()
 
@@ -121,7 +229,7 @@ class MainActivity : AppCompatActivity() {
             @Throws(IOException::class)
             override fun onResponse(call: Call, response: Response) {
                 val myResponse: String = response.body()!!.string()
-
+                var dateMomoModel = mapper.readValue<DateMomoModel>(myResponse)
             }
         })
     }
