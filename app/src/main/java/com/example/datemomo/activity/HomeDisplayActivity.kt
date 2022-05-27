9a5306c45a0b72cc1916b5183493fd3b5f447e65
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2022-05-27 14:15:09 +0300

    Hid both navigation and status bars, so that my custom bottom navigation layout can perfectly sit at the bottom of the screen

--- app/src/main/java/com/example/datemomo/activity/HomeDisplayActivity.kt
+++ app/src/main/java/com/example/datemomo/activity/HomeDisplayActivity.kt
@@ -2,7 +2,7 @@ package com.example.datemomo.activity
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
@@ -37,11 +37,34 @@ class HomeDisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
         binding = ActivityHomeDisplayBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         window.setStatusBarDarkIcons(true)
         window.setNavigationBarDarkIcons(true)
+
+        window.decorView.systemUiVisibility = flags
+
+        // Code below is to handle presses of Volume up or Volume down.
+        // Without this, after pressing volume buttons, the navigation bar will
+        // show up and won't hide
+
+        // Code below is to handle presses of Volume up or Volume down.
+        // Without this, after pressing volume buttons, the navigation bar will
+        // show up and won't hide
+        val decorView = window.decorView
+        decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+                if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                    decorView.systemUiVisibility = flags
+                }
+            }
 
         val displayMetrics = DisplayMetrics()
 
--- app/src/main/java/com/example/datemomo/activity/UserBioActivity.kt
+++ app/src/main/java/com/example/datemomo/activity/UserBioActivity.kt
@@ -35,11 +35,34 @@ class UserBioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
         binding = ActivityUserBioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         window.setStatusBarDarkIcons(true)
         window.setNavigationBarDarkIcons(true)
+
+        window.decorView.systemUiVisibility = flags
+
+        // Code below is to handle presses of Volume up or Volume down.
+        // Without this, after pressing volume buttons, the navigation bar will
+        // show up and won't hide
+
+        // Code below is to handle presses of Volume up or Volume down.
+        // Without this, after pressing volume buttons, the navigation bar will
+        // show up and won't hide
+        val decorView = window.decorView
+        decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+                if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                    decorView.systemUiVisibility = flags
+                }
+            }
 
         buttonClickEffect = AlphaAnimation(1f, 0f)
         sharedPreferences =
