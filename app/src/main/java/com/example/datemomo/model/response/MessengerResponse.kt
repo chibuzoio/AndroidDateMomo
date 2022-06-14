9951c9ebef277675767eb34fc62ae86f411ba2fe
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2022-06-14 18:22:12 +0300

    Made changes to MessengerResponse data class to correspond to the server messenger response class

--- app/src/main/java/com/example/datemomo/model/response/MessengerResponse.kt
+++ app/src/main/java/com/example/datemomo/model/response/MessengerResponse.kt
@@ -3,12 +3,14 @@ package com.example.datemomo.model.response
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MessengerResponse(var memberId: Int,
+data class MessengerResponse(var chatmateId: Int,
+                             var messengerId: Int,
                              var fullName: String,
                              var userName: String,
                              var lastMessage: String,
                              var profilePicture: String,
                              var lastMessageDate: String,
-                             var unreadMessageCount: Int)
+                             var unreadMessageCount: Int,
+                             var messageTableName: String)
 
 
